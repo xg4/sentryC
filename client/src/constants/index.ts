17aae4c5fef4c620c2c639da09fd554240ab7caa
xg4 <xingor4@gmail.com>
2025-02-24 09:01:21 +0000

    feat: remove client

--- client/src/constants/index.ts
+++
@@ -1,3 +0,0 @@
-import dayjs from 'dayjs'
-
-export const today = [dayjs().startOf('d').toISOString(), dayjs().endOf('d').toISOString()]
--- client/src/utils/request.ts
+++
@@ -1,4 +0,0 @@
-import { hc } from 'hono/client'
-import type { ApiRoutes } from '../../../server/app'
-
-export const client = hc<ApiRoutes>('/')
